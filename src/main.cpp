1d2921e306d58ac78ee8ecdfec80578a51dc73d3
anatols <anatol.sukhomlin@gmail.com>
2024-04-21 20:43:25 +0300

    New project created

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+// This example uses an Adafruit Huzzah ESP8266
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://www.shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+#include "Ticker.h"
+
+const char ssid[] = "dd-wrt";
+const char pass[] = "19731998";
+
+WiFiClient net;
+MQTTClient client;
+Ticker alarm;
+
+
+unsigned long lastMillis = 0;
+
+int ledPin = 2; 
+#define in_led 0
+
+void changeState()
+{
+  digitalWrite(in_led,!digitalRead(in_led));
+}
+
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "", "")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("zgwAA54/alarm");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  if(payload.startsWith("leek=0")) {
+    Serial.print("Leek is stopped!\n");
+    alarm.detach();
+  }
+
+  if(payload.startsWith("leek=1")) {
+    Serial.print("Leek is started!\n");
+    alarm.attach(0.5,changeState);
+  }
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  client.begin("10.180.10.144", net);
+  client.onMessage(messageReceived);
+  pinMode(in_led, OUTPUT);
+  digitalWrite(in_led, LOW);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 10000) {
+    lastMillis = millis();
+    client.publish("zgwAA54/Siren", "I'm here");
+  }
+}
